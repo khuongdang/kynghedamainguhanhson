ef6cd52b10d415cee89df9e16b38553eb685735d
khuongdd <khuongdd@evolableasia.vn>
2016-05-05 12:30:28 +0700

    update new code

--- wp-content/themes/nhathanhphat/index.php
+++ wp-content/themes/nhathanhphat/index.php
@@ -1,146 +1,87 @@
-<?php get_header(); ?>
+<?php get_header();
+$muabannha = get_post_from_post_type(null, 'muabannhadat');
+$taxonomy = 'danh-muc-san-pham';
+$terms = get_terms($taxonomy, array(
+    'hide_empty' => 0,
+) );
+shuffle ($terms);
+$tintuc = get_post_from_post_type(null, 'tintuc', 'ID DESC');
+$tin_dau = $tintuc[0];
+$title_tin_dau = $tin_dau->post_title;
+$link_tin_dau = get_permalink($tin_dau->ID);
+$img_tin_dau = wp_get_attachment_image_src(get_post_thumbnail_id($tin_dau->ID), 'single-post-thumbnail');
+$img_tin_dau = $img_tin_dau[0];
+$tintucother =  get_post_from_post_type($tin_dau->ID, 'tintuc');
+?>
             <!-- main -->
             <div class="main">
                 <a href="#" class="m-btn-grey grey-btn">request a demo</a>
                 <section class="cols">
-
                     <div class="col">
-                        <div class="ca-home-head"><a href="/dich-vu-luat-su.htm" title="Dịch vụ luật sư" class=""><img
-                                    src="http://luattritam.com.vn/uploads/Icon/icon_dich_vu_luat.jpg"
-                                    alt="Dịch vụ luật sư" title="Dịch vụ luật sư"/></a>
-                            <h2><a href="/dich-vu-luat-su.htm" title="Dịch vụ luật sư" class="head-title">Dịch vụ luật
-                                    sư</a></h2></div>
+                        <div class="ca-home-head"><a href="/mua-ban-nha-dat/" title="Mua bán nhà đất" class=""><img width="70" height="70"
+                                    src="<?php echo get_template_directory_uri();?>/images/house.png"
+                                    alt="Mua bán nhà đất" title="Mua bán nhà đất"/></a>
+                            <h2><a href="/mua-ban-nha-dat/" title="Mua bán nhà đất" class="head-title">Mua bán nhà đất</a></h2></div>
                         <div class="col-cnt h-list-ca">
                             <ul>
-                                <li><a title="Tư vấn thành lập Công ty" href="/tu-van-thanh-lap-cong-ty.htm"> Tư vấn
-                                        thành lập Công ty </a></li>
-                                <li><a title="Tư vấn doanh nghiệp" href="/tu-van-doanh-nghiep.htm"> Tư vấn doanh
-                                        nghiệp </a></li>
-                                <li><a title="Sở hữu trí tuệ" href="/so-huu-tri-tue.htm"> Sở hữu trí tuệ </a></li>
-                                <li><a title="Tư vấn đất đai" href="/tu-van-dat-dai.htm"> Tư vấn đất đai </a></li>
-                                <li><a title="Tư vấn Thừa kế - Di chúc" href="/tu-van-thua-ke-di-chuc.htm"> Tư vấn Thừa
-                                        kế - Di chúc </a></li>
-                                <li><a title="Tư vấn Hôn nhân Gia đình" href="/tu-van-hon-nhan-gia-dinh.htm"> Tư vấn Hôn
-                                        nhân Gia đình </a></li>
-                                <li><a title="Tư vấn soạn thảo hợp đồng" href="/tu-van-soan-thao-hop-dong.htm"> Tư vấn
-                                        soạn thảo hợp đồng </a></li>
-                                <li><a title="Tư vấn Luật lao động và việc làm"
-                                       href="/tu-van-luat-lao-dong-va-viec-lam.htm"> Tư vấn Luật lao động và việc
-                                        làm </a></li>
-                                <li><a title="Dịch vụ về Tranh Tụng" href="/dich-vu-ve-tranh-tung.htm"> Dịch vụ về Tranh
-                                        Tụng </a></li>
+                                <?php foreach ($muabannha as $obj) {
+                                    $title  = $obj->post_title;
+                                    $link   = get_permalink($obj->ID);
+                                    ?>
+                                <li><a title="<?php echo $title;?>" href="<?php echo $link;?>"> <?php echo $title;?> </a></li>
+                                <?php }?>
                             </ul>
                         </div>
                     </div>
+                    <?php foreach ($terms as $obj) {
+                        $title = $obj->name;
+                        $link = get_term_link($obj->term_id);
+                        $cat_img_url = z_taxonomy_image_url($obj->term_id);
+                        ?>
                     <div class="col">
-                        <div class="ca-home-head"><a href="/dich-vu-cap-phep.htm" title="Dịch vụ cấp phép" class=""><img
-                                    src="http://luattritam.com.vn/uploads/Icon/icon_dich_vu_cap_phep.jpg"
-                                    alt="Dịch vụ cấp phép" title="Dịch vụ cấp phép"/></a>
-                            <h2><a href="/dich-vu-cap-phep.htm" title="Dịch vụ cấp phép" class="head-title">Dịch vụ cấp
-                                    phép</a></h2></div>
+                        <div class="ca-home-head"><a href="" title="<?php echo $title;?>" class=""><img width="80" height="80"
+                                    src="<?php echo $cat_img_url;?>"
+                                    alt="<?php echo $title;?>" title="<?php echo $title;?>"/></a>
+                            <h2><a href="<?php echo $link;?>" title="<?php echo $title;?>" class="head-title"><?php echo $title;?></a></h2></div>
                         <div class="col-cnt h-list-ca">
                             <ul>
-                                <li><a title="Cấp phép đầu tư" href="/cap-phep-dau-tu.htm"> Cấp phép đầu tư </a></li>
-                                <li><a title="Thủ tục hành chính cho Doanh nghiệp"
-                                       href="/thu-tuc-hanh-chinh-cho-doanh-nghiep.htm"> Thủ tục hành chính cho Doanh
-                                        nghiệp </a></li>
-                                <li><a title="Công bố mỹ phẩm" href="/cong-bo-my-pham.htm"> Công bố mỹ phẩm </a></li>
-                                <li><a title="Công bố thực phẩm" href="/cong-bo-thuc-pham.htm"> Công bố thực phẩm </a>
-                                </li>
-                                <li><a title="Dịch vụ tư vấn cấp sổ đỏ" href="/dich-vu-tu-van-cap-so-do.htm"> Dịch vụ tư
-                                        vấn cấp sổ đỏ </a></li>
-                                <li><a title="Giấy phép về web" href="/giay-phep-ve-web.htm"> Giấy phép về web </a></li>
-                                <li><a title="Giấy phép ATVSTP" href="/giay-phep-atvstp.htm"> Giấy phép ATVSTP </a></li>
-                                <li><a title="Làm thủ tục xin lý lịch tư pháp"
-                                       href="/lam-thu-tuc-xin-ly-lich-tu-phap.htm"> Làm thủ tục xin lý lịch tư pháp </a>
-                                </li>
-                                <li><a title="Hợp pháp hóa lãnh sự, chứng nhận lãnh sự"
-                                       href="/hop-phap-hoa-lanh-su-chung-nhan-lanh-su.htm"> Hợp pháp hóa lãnh sự, chứng
-                                        nhận lãnh sự </a></li>
+                                <?php
+                                $sub_content = get_post_from_taxonomy('san-pham', 'danh-muc-san-pham', $obj->term_id);
+                                foreach ($sub_content as $obj) {
+                                    $sub_title = $obj->post_title;
+                                    $sub_link = get_permalink($obj->ID);
+                                    ?>
+                                <li><a title="<?php echo $sub_title;?>" href="<?php echo $sub_link;?>"> <?php echo $sub_title;?> </a></li>
+                                <?php }?>
                             </ul>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="ca-home-head"><a href="/dich-vu-cho-nguoi-nuoc-ngoai.htm"
-                                                     title="Dịch vụ cho người nước ngoài" class=""><img
-                                    src="http://luattritam.com.vn/uploads/Icon/icon_dich_vu_cho_nguoi_nuoc_ngoai.jpg"
-                                    alt="Dịch vụ cho người nước ngoài" title="Dịch vụ cho người nước ngoài"/></a>
-                            <h2><a href="/dich-vu-cho-nguoi-nuoc-ngoai.htm" title="Dịch vụ cho người nước ngoài"
-                                   class="head-title">Dịch vụ cho người nước ngoài</a></h2></div>
-                        <div class="col-cnt h-list-ca">
-                            <ul>
-                                <li><a title="Cấp giấy phép lao động cho người nước ngoài"
-                                       href="/cap-giay-phep-lao-dong-cho-nguoi-nuoc-ngoai.htm"> Cấp giấy phép lao động
-                                        cho người nước ngoài </a></li>
-                                <li><a title="Dịch vụ hỗ trợ làm thẻ tạm trú"
-                                       href="/dich-vu-ho-tro-lam-the-tam-tru.htm"> Dịch vụ hỗ trợ làm thẻ tạm trú </a>
-                                </li>
-                                <li><a title="Dịch vụ dành cho Việt Kiều" href="/dich-vu-danh-cho-viet-kieu.htm"> Dịch
-                                        vụ dành cho Việt Kiều </a></li>
-                                <li><a title="Dịch vụ Đăng ký kết hôn với người Việt Nam"
-                                       href="/dich-vu-dang-ky-ket-hon-voi-nguoi-viet-nam.htm"> Dịch vụ Đăng ký kết hôn
-                                        với người Việt Nam </a></li>
-                                <li><a title="Dịch vụ miễn thị thực" href="/dich-vu-mien-thi-thuc.htm"> Dịch vụ miễn thị
-                                        thực </a></li>
-                                <li><a title="Dịch vụ làm visa Việt Nam" href="/dich-vu-lam-visa-viet-nam.htm"> Dịch vụ
-                                        làm visa Việt Nam </a></li>
-                                <li><a title="Tư vấn làm thẻ APEC" href="/tu-van-lam-the-apec.htm"> Tư vấn làm thẻ
-                                        APEC </a></li>
-                            </ul>
-                        </div>
-                    </div>
-
+                    <?php }?>
                 </section>
                 <section class="post">
                     <div class="h-news-bot-box">
                         <div class="h-head-news"><h2>Tin tức</h2></div>
                         <div class="video-holder">
-                            <a href="/tin-chuyen-nganh/nhung-diem-moi-cua-luat-dau-tu-nam-2015.htm"><img
-                                    src="http://luattritam.com.vn/uploads/image/nhung-diem-moi-cua-luat-dau-tu-2015.jpg"
-                                    alt="Những điểm mới của Luật đầu tư năm 2015"
-                                    title="Những điểm mới của Luật đầu tư năm 2015"/></a>
+                            <a href=""><img
+                                    src="<?php echo $img_tin_dau;?>"
+                                    alt="<?php echo $title_tin_dau;?>"
+                                    title="<?php echo $title_tin_dau;?>"/></a>
                         </div>
                         <div class="post-cnt">
 
                             <h2 class="h-news-view"><a
-                                    href="/tin-chuyen-nganh/nhung-diem-moi-cua-luat-dau-tu-nam-2015.htm"
-                                    title="Những điểm mới của Luật đầu tư năm 2015" class="head-title">Những điểm mới
-                                    của Luật đầu tư năm 2015</a></h2>
+                                    href="<?php echo $link_tin_dau;?>"
+                                    title="<?php echo $title_tin_dau;?>" class="head-title"><?php echo $title_tin_dau;?></a></h2>
                             <p class="lineh17">&#160;</p>
                         </div>
                         <ul>
-
-                            <li><a href="/tin-chuyen-nganh/nhung-diem-moi-cua-luat-doanh-nghiep-sua-doi-nam-2014.htm"
-                                   title="Những  điểm mới của Luật doanh nghiệp sửa đổi năm 2014">Những điểm mới của
-                                    Luật doanh nghiệp sửa đổi năm 2014</a></li>
-
-                            <li><a href="/tin-chuyen-nganh/se-bo-thu-tuc-xin-giay-chung-nhan-dau-tu.htm"
-                                   title="Sẽ bỏ thủ tục xin giấy chứng nhận đầu tư">Sẽ bỏ thủ tục xin giấy chứng nhận
-                                    đầu tư</a></li>
-
-                            <li><a href="/tin-chuyen-nganh/diem-moi-cua-luat-dau-thau-2013.htm"
-                                   title="Điểm mới của Luật Đấu thầu 2013">Điểm mới của Luật Đấu thầu 2013</a></li>
-
-                            <li>
-                                <a href="/tin-chuyen-nganh/doanh-nghiep-moi-thanh-lap-can-nhung-luu-y-nhung-gi-ve-van-de-thue.htm"
-                                   title="Doanh nghiệp  mới thành lập, cần những  lưu ý những gì về vấn đề thuế?">Doanh
-                                    nghiệp mới thành lập, cần những lưu ý những gì về vấn đề thuế?</a></li>
-
-                            <li>
-                                <a href="/tin-chuyen-nganh/dieu-kien-mua-hoa-don-vat-voi-doanh-nghiep-thanh-lap-nam-2014.htm"
-                                   title="Điều kiện mua hóa đơn VAT với doanh nghiệp thành lập năm 2014 ">Điều kiện mua
-                                    hóa đơn VAT với doanh nghiệp thành lập năm 2014 </a></li>
-
-                            <li>
-                                <a href="/tin-chuyen-nganh/doanh-nghiep-phai-mang-nhung-hoa-don-chung-tu-gi-khi-luu-thong-hang-hoa-tren-duong.htm"
-                                   title=" Doanh nghiệp  phải mang những hóa đơn, chứng từ gì khi lưu thông hàng hóa trên đường">
-                                    Doanh nghiệp phải mang những hóa đơn, chứng từ gì khi lưu thông hàng hóa trên
-                                    đường</a></li>
-
-                            <li><a href="/tin-chuyen-nganh/thu-tuc-phap-ly-khi-thay-doi-tru-so-cong-ty.htm"
-                                   title="Thủ tục pháp lý khi thay đổi trụ sở công ty  ">Thủ tục pháp lý khi thay đổi
-                                    trụ sở công ty </a></li>
-
+                            <?php foreach($tintucother as $obj){
+                                $link_tin = get_permalink($obj->ID);
+                                $title_tin = $obj->post_title;
+                                ?>
+                            <li><a href="<?php echo $link_tin;?>"
+                                   title="<?php echo $title_tin;?>"><?php echo $title_tin;?></a></li>
+                            <?php }?>
                         </ul>
                     </div>
 
@@ -224,7 +165,7 @@
                         <div class="h-head-image"><h2>Hồ sơ năng lực</h2></div>
                         <div class="hoso-image-ga">
 
-                            <a href="http://luattritam.com.vn/uploads/HosonanglucTriTam.pdf"><img alt="Hồ sơ năng lực"
+                            <a href="#"><img alt="Hồ sơ năng lực"
                                                                                                   title="Hồ sơ năng lực"
                                                                                                   src="<?php echo get_template_directory_uri(); ?>/images/hosonangluc.jpg"/></a>
 
